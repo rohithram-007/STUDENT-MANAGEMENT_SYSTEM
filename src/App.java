50d247bccd9c055be8fbfb5aafcdd98391a19101
rohithram-007 <jrohithram@gmail.com>
2025-10-29 23:58:41 +0530

    second commit

--- src/App.java
+++ src/App.java
@@ -2,7 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
         for(int i=0;i<10;i++){
-            System.out.println("Hello world");
+            System.out.println("I Hate World");
+
              }
     }
 }
